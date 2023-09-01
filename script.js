a012d360613d1e9887e09999606da0fa9a2e2249
saikumarchittaluri <127985962+saikumarchittaluri@users.noreply.github.com>
2023-09-01 22:53:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your code here
+document.addEventListener('DOMContentLoaded', function () {
+    const images = document.querySelectorAll('.image');
+    let dragItem = null;
+
+    images.forEach((image) => {
+        image.addEventListener('dragstart', (e) => {
+            dragItem = e.target;
+        });
+
+        image.addEventListener('dragover', (e) => {
+            e.preventDefault();
+        });
+
+        image.addEventListener('drop', (e) => {
+            e.preventDefault();
+            if (dragItem && dragItem !== e.target) {
+                // Swap background images
+                const temp = e.target.style.backgroundImage;
+                e.target.style.backgroundImage = dragItem.style.backgroundImage;
+                dragItem.style.backgroundImage = temp;
+            }
+        });
+    });
+});
